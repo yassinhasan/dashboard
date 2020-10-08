1e21ab5c2b842ba97aa5139ca2d522362b19d3a5
yassinhasan <figo781@gmail.com>
2020-10-08 19:52:03 +0300

    structure

---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+
+const gulp = require('gulp');
+const concat = require('gulp-concat');
+const autoprefixer = require('gulp-autoprefixer');
+const sass = require('gulp-sass');
+const GulpPug = require('gulp-pug');
+const sourcemaps = require('gulp-sourcemaps');
+const notify = require("gulp-notify");
+const minify = require('gulp-minify');
+
+gulp.task('html',function(){
+    return gulp.src('stage/html/*.pug')
+     .pipe(sourcemaps.init())
+     .pipe(GulpPug({pretty: true}))
+     .pipe(sourcemaps.write('.'))
+     .pipe(gulp.dest('dist'))
+     
+ });
+
+gulp.task('css',function(){
+    return gulp.src(['stage/css/**/*.scss','stage/css/**/*.css'])
+     .pipe(sourcemaps.init())
+     .pipe(sass({outputStyle: "compressed"}).on('error',sass.logError))
+     .pipe(autoprefixer())
+     .pipe(concat('main.css'))
+     .pipe(sourcemaps.write('.')) //في مكان جنب المكان الي انا عملت فيه كونكات دي فكره الدوت
+     .pipe(gulp.dest('dist/css'))
+     
+ });
+ gulp.task('js',function(){
+    return gulp.src('stage/js/**/*.js')
+     .pipe(concat('main.js'))
+     .pipe(minify())
+     .pipe(gulp.dest('dist/js'))
+     
+ });
+ gulp.task('watch',function(){
+    gulp.watch(['stage/html/**/*.pug','stage/css/**/*.css','stage/css/**/*.scss','stage/js/**/*.js'],gulp.series('html','css','js'))
+});
