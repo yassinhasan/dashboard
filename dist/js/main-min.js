697ac2ee9b58ed0182505ee1c3428b5f1432fdfd
yassinhasan <figo781@gmail.com>
2020-10-10 16:27:48 +0300

    continus

---
+++ dist/js/main-min.js
@@ -0,0 +1,1 @@
+$(function(){$(".toogle-sidebar").on("click",function(){$(this).toggleClass("fa-arrow-left  fa-arrow-right"),$(".side-bar").toggleClass("no-sidebar"),$(".content-area").toggleClass("full-width")})});
