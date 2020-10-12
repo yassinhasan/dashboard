6222a89a0d5ec35aec5191ce4eeb97ff86e4ed3b
yassinhasan <figo781@gmail.com>
2020-10-12 13:33:32 +0300

    sidebar

--- dist/js/main-min.js
+++ dist/js/main-min.js
@@ -1,1 +1,1 @@
-$(function(){$(".toogle-sidebar").on("click",function(){$(this).toggleClass("fa-arrow-left  fa-arrow-right"),$(".side-bar").toggleClass("no-sidebar"),$(".content-area").toggleClass("full-width")})});
+$(function(){$(".toogle-sidebar").on("click",function(){$(this).toggleClass("fa-arrow-left  fa-arrow-right"),$(".side-bar").toggleClass("no-sidebar"),$(".content-area").toggleClass("full-width")}),$(".links-area > li >  a").on("click",function(){$(this).find($("i")).toggleClass("fa-angle-right  fa-angle-down"),$(this).next($(".child-links")).slideToggle(300)})});
--- dist/js/main.js
+++ dist/js/main.js
@@ -3,5 +3,10 @@ $(function(){
         $(this).toggleClass('fa-arrow-left  fa-arrow-right');
         $('.side-bar').toggleClass('no-sidebar')
         $('.content-area').toggleClass('full-width')
-    })
+    });
+    $('.links-area > li >  a').on('click',function(){
+        $(this).find($('i')).toggleClass('fa-angle-right  fa-angle-down');
+        $(this).next($('.child-links')).slideToggle(300)
+    });
+
 })
