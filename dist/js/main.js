22847962af91b772469bf47e23ff5f5d16318131
yassinhasan <figo781@gmail.com>
2020-10-14 13:34:32 +0300

    =icons

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,12 +1,49 @@
+/* Get the documentElement (<html>) to display the page in fullscreen */
+var elem = document.documentElement;
+
+/* View in fullscreen */
+function openFullscreen() {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.mozRequestFullScreen) { /* Firefox */
+    elem.mozRequestFullScreen();
+  } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { /* IE/Edge */
+    elem.msRequestFullscreen();
+  }
+}
+
+/* Close fullscreen */
+function closeFullscreen() {
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.mozCancelFullScreen) { /* Firefox */
+    document.mozCancelFullScreen();
+  } else if (document.webkitExitFullscreen) { /* Chrome, Safari and Opera */
+    document.webkitExitFullscreen();
+  } else if (document.msExitFullscreen) { /* IE/Edge */
+    document.msExitFullscreen();
+  }
+}
 $(function(){
     $('.toogle-sidebar').on('click',function(){
-        $(this).toggleClass('fa-arrow-left  fa-arrow-right');
         $('.side-bar').toggleClass('no-sidebar')
         $('.content-area').toggleClass('full-width')
     });
     $('.links-area > li >  a').on('click',function(){
         $(this).find($('i')).toggleClass('fa-angle-right  fa-angle-down');
-        $(this).next($('.child-links')).slideToggle(300)
+        $(this).next($('.child-links')).slideToggle(500)
     });
+    $('.full-screen').on('click',function(){
+        $(this).toggleClass('active');
+        if($(this).hasClass('active')){
+            openFullscreen();
+        }
+        else{
+            closeFullscreen()
+        }
+        
+    })
 
 })
